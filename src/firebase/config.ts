3e6664f82c6eec77aee0696ff9350f7765277e67
Thomas Wang <thomastieyi33213@gmail.com>
2025-12-23 02:47:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8714277206-9b646",
+  "appId": "1:1092528813716:web:578af424f29b698dcf2681",
+  "apiKey": "AIzaSyBQ4WOJrJMc3id9Qw-7F2UFsJW0b6SExgc",
+  "authDomain": "studio-8714277206-9b646.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1092528813716"
+};
